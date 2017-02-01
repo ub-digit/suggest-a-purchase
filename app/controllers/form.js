8c3c64eca4c5b34191770bca8012d26e331414d3
Magnus Kull <magnus.kull@ub.gu.se>
2017-02-01 15:23:11 +0100

    more forms

--- app/controllers/form.js
+++ app/controllers/form.js
@@ -4,22 +4,31 @@ export default Ember.Controller.extend({
 
   selectedOrderType: null,
   showConfirmation: false,
+  contactOptOut: false,
 
   selectedOrderTypeIsBook: Ember.computed.equal('selectedOrderType', 'book'),
   selectedOrderTypeIsJournal: Ember.computed.equal('selectedOrderType', 'journal'),
   selectedOrderTypeIsDatabase: Ember.computed.equal('selectedOrderType', 'database'),
   selectedOrderTypeIsOther: Ember.computed.equal('selectedOrderType', 'other'),
 
+  showContactInputs: Ember.computed.equal('contactOptOut', false),
+
   order: {
     book: {
       title: null,
-      author: null
+      author: null,
+      isbn: null,
+      year: null,
+      other: null
     },
     journal: {
-      title: null
+      title: null,
+      issn: null,
+      other: null
     },
     database: {
-      title: null
+      title: null,
+      other: null
     },
     other: {
       description: null
